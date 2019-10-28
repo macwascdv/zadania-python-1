3edc4ee6ad3be6b36aca68871a67d5d89804a18f
Maciej Wąsiatycz <56920706+macwascdv@users.noreply.github.com>
2019-10-28 23:25:15 +0100

    Update zadanie 3.py

--- zadanie 3.py
+++ zadanie 3.py
@@ -1,13 +1,23 @@
 print("Ten program zamień temperaturę na inna skale")
-temperatura = input("Wybierz skalę początkową - f/c: ")
-x= int(input("wpisz temperaturę: "))
-if (temperatura=="f"):
-    y=(x-32)*(5/9)
-    print("Twoja temperatura w Celsjuszach wynosi: ")
-    print("{} stopni Celsjusza".format(y))
-    input()
-elif (temperatura=="c"):
-    y=(x*9/5)+32
-    print("Twoja temperatura wynosi: ")
-    print("{} Fahrenheit".format(y))
-    input()
+decyzja="t"
+while(decyzja=="t"):
+ try:
+    temperatura = input("Wybierz skalę początkową - f/c: ")
+    if (temperatura=="f"):
+           x= int(input("wpisz temperaturę: "))
+           y=(x-32)*(5/9)
+           print("Twoja temperatura w Celsjuszach wynosi: ")
+           print("{:.2f} stopni Celsjusza".format(y))
+           decyzja=input("Czy chcesz ponownie uruchomic program? t/n ")
+    elif (temperatura=="c"):
+        x= int(input("wpisz temperaturę: "))
+        y=(x*9/5)+32
+        print("Twoja temperatura wynosi: ")
+        print("{:.2f} Fahrenheit".format(y))
+        decyzja=input("Czy chcesz ponownie uruchomic program? t/n ")
+    else:
+        print("Musisz wpisac f lub c")
+        decyzja=input("Czy chcesz sprobowac ponownie? t/n ")
+ except ValueError:
+     print("Podano inną wartosc niz liczba")
+     input("Czy chcesz sprobowac ponownie? t/n ")
