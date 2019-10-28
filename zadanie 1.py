22ff4c0a09c50def4ef0bb5fca3f10d8ef2aa892
Maciej Wąsiatycz <56920706+macwascdv@users.noreply.github.com>
2019-10-28 22:35:22 +0100

    Update zadanie 1.py
    *Wprowadzenie funkcji powtórzenia działania przy podaniu błędnych wartości

--- zadanie 1.py
+++ zadanie 1.py
@@ -1,5 +1,10 @@
-x=int(input("wprowadz liczbe: "))
-for i in range (1,x+1):
-    if(x%i==0):
-        print(i)
-input("Nacisnij dowolny klawisz")
+decyzja="t"
+while(decyzja=="t"):
+    try:
+         x=int(input("Wprowadz liczbe: "))
+         for i in range (1, x+1):
+             if((x%i)==0):
+                 print(i)
+    except ValueError:
+        print("Musisz wprowadzić liczbę")
+        decyzja=input("Czy chcesz powtórzyć? t/n ")
