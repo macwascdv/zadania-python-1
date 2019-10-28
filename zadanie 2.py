8469f77b08a9e1b423f20c66aa3065917f9c2d62
Maciej Wąsiatycz <56920706+macwascdv@users.noreply.github.com>
2019-10-28 23:00:12 +0100

    Update zadanie 2.py

--- zadanie 2.py
+++ zadanie 2.py
@@ -1,5 +1,13 @@
-x=int(input("Wprowadz liczbe: "))
-for i in range(0, x+1):
-    x1=float(x)
-    print("{0}x{1}={2}".format(x1, i, x1*i))
-input("Naciśnij dowolny klawisz, by wyjść")
+print("Program do...Nie wiem własciwie po co")
+decyzja="t"
+while(decyzja=="t"):
+    try:
+        x=int(input("wprowadz liczbe:"))
+        for i in range(0, 11):
+            x1=float(x)
+            a=x1*i
+            print("{}x{}={}".format(x1,i,a))
+    except ValueError:
+        print("Musisz podac liczbe")
+        decyzja=input("Czy chcesz powtorzyc? t/n ")
+        
